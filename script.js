cf5600ea96f22aa830c3b91f483b36a8ea7445e9
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2022-06-13 22:11:12 +0100

    Update script.js
    To make sure your counter variable maintain the incremental value, this variable should be a global variable.

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
+let count = 0;
 
- let makecard = () => {   
-         
-    let count = 0; 
-
-    if(count>=6) {
-        alert("Você atingiu a quantidade máxima de cards!");
-     } else { 
-    let title = document.querySelector('#input-titulo').value;
-    let describe = document.querySelector('#input-descricao').value; 
-    let image = document.querySelector('#input-url').value; 
+let makecard = () => {
+  if (count >= 6) {
+    alert("Você atingiu a quantidade máxima de cards!");
+  } else {
+    let title = document.querySelector("#input-titulo").value;
+    let describe = document.querySelector("#input-descricao").value;
+    let image = document.querySelector("#input-url").value;
     let cardArea = document.querySelector(".card-area");
 
-    const makeCard =  `
+    const makeCard = `
     <div class="card"> 
         <img src="${image}" alt="imagem do tênis">
         <h3>${title}<h3>
         <p>${describe}<p>
     </div>
     `;
-    
-    cardArea.innerHTML+=makeCard;   
-    count++; 
-    console.log(count);
-}   
-     }
 
-       
+    cardArea.innerHTML += makeCard;
+    count++;
+    console.log(count);
+  }
+};
