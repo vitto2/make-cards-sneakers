7ad2df800de808d4f81ccd07c5b909be8c4088d2
vitto2 <vitor2071@gmail.com>
2022-06-13 17:42:38 -0300

    subindo atualização do projeto. 90% concluído.

---
+++ script.js
@@ -0,0 +1,26 @@
+
+ let makecard = () => {   
+         
+    let count = 0; 
+
+    if(count>=6) {
+        alert("Você atingiu a quantidade máxima de cards!");
+     } else { 
+    let title = document.querySelector('#input-titulo').value;
+    let describe = document.querySelector('#input-descricao').value; 
+    let image = document.querySelector('#input-url').value; 
+    let cardArea = document.querySelector(".card-area");
+
+    const makeCard =  `
+    <div class="card"> 
+        <img src="${image}" alt="imagem do tênis">
+        <h3>${title}<h3>
+        <p>${describe}<p>
+    </div>
+    `;
+    
+    cardArea.innerHTML+=makeCard;   
+}   
+     }
+
+       
